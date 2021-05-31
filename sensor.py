02d496b158414d7cfa757c7b97250f9b5fdaa854
Pongsakorn U-chupala <puchupala@gmail.com>
2021-05-31 23:45:49 +0900

    Implement temperature, humidity, and illuminance sensor

--- sensor.py
+++ sensor.py
@@ -6,8 +6,14 @@ from homeassistant.const import (
     ENERGY_KILO_WATT_HOUR,
     POWER_WATT,
     DEVICE_CLASS_POWER,
+    TEMP_CELSIUS,
+    DEVICE_CLASS_TEMPERATURE,
+    PERCENTAGE,
+    DEVICE_CLASS_HUMIDITY,
+    LIGHT_LUX,
+    DEVICE_CLASS_ILLUMINANCE,
 )
-from . import DOMAIN, NatureRemoBase
+from . import DOMAIN, NatureRemoBase, NatureRemoDeviceBase
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,13 +25,17 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     _LOGGER.debug("Setting up sensor platform.")
     coordinator = hass.data[DOMAIN]["coordinator"]
     appliances = coordinator.data["appliances"]
-    async_add_entities(
-        [
-            NatureRemoE(coordinator, appliance)
-            for appliance in appliances.values()
-            if appliance["type"] == "EL_SMART_METER"
-        ]
-    )
+    devices = coordinator.data["devices"]
+    entities = [
+        NatureRemoE(coordinator, appliance)
+        for appliance in appliances.values()
+        if appliance["type"] == "EL_SMART_METER"
+    ]
+    for device in devices.values():
+        entities.append(NatureRemoTemperatureSensor(coordinator, device))
+        entities.append(NatureRemoHumiditySensor(coordinator, device))
+        entities.append(NatureRemoIlluminanceSensor(coordinator, device))
+    async_add_entities(entities)
 
 
 class NatureRemoE(NatureRemoBase):
@@ -69,3 +79,90 @@ class NatureRemoE(NatureRemoBase):
         Only used by the generic entity update service.
         """
         await self._coordinator.async_request_refresh()
+
+
+class NatureRemoTemperatureSensor(NatureRemoDeviceBase):
+    """Implementation of a Nature Remo sensor."""
+
+    def __init__(self, coordinator, appliance):
+        super().__init__(coordinator, appliance)
+        self._name = self._name.strip() + " Temperature"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return self._device["id"] + "-te"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return TEMP_CELSIUS
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        device = self._coordinator.data["devices"][self._device["id"]]
+        return device["newest_events"]["te"]["val"]
+
+    @property
+    def device_class(self):
+        """Return the device class."""
+        return DEVICE_CLASS_TEMPERATURE
+
+
+class NatureRemoHumiditySensor(NatureRemoDeviceBase):
+    """Implementation of a Nature Remo sensor."""
+
+    def __init__(self, coordinator, appliance):
+        super().__init__(coordinator, appliance)
+        self._name = self._name.strip() + " Humidity"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return self._device["id"] + "-hu"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return PERCENTAGE
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        device = self._coordinator.data["devices"][self._device["id"]]
+        return device["newest_events"]["hu"]["val"]
+
+    @property
+    def device_class(self):
+        """Return the device class."""
+        return DEVICE_CLASS_HUMIDITY
+
+
+class NatureRemoIlluminanceSensor(NatureRemoDeviceBase):
+    """Implementation of a Nature Remo sensor."""
+
+    def __init__(self, coordinator, appliance):
+        super().__init__(coordinator, appliance)
+        self._name = self._name.strip() + " Illuminance"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return self._device["id"] + "-il"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return LIGHT_LUX
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        device = self._coordinator.data["devices"][self._device["id"]]
+        return device["newest_events"]["il"]["val"]
+
+    @property
+    def device_class(self):
+        """Return the device class."""
+        return DEVICE_CLASS_ILLUMINANCE 
